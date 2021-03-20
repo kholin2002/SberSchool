2954133964218730115df6fe888cd586975c7ecc
George Kholin <kholin2002@gmail.com>
2021-03-20 22:12:06 +0300

    lesson 4

--- src/main/java/ru/sber/FileConverter.java
+++ src/main/java/ru/sber/FileConverter.java
@@ -6,29 +6,29 @@ public class FileConverter {
 
     @FunctionalInterface
     public interface CharConverter {
-        int convert(int value);
+        int convertChar(int value);
     }
 
-    public static void Convert(Reader in, Writer out, CharConverter c) throws IOException {
+    public static void convert(Reader in, Writer out, CharConverter c) throws IOException {
         int nextChar = in.read();
         while (nextChar >= 0) {
-            out.write(c.convert(nextChar));
+            out.write(c.convertChar(nextChar));
             nextChar = in.read();
         }
     }
 
-    public static void Convert(String inFileName, String outFileName, CharConverter c) throws IOException {
+    public static void convert(String inFileName, String outFileName, CharConverter c) throws IOException {
         try (FileReader in = new FileReader(inFileName)) {
             try (FileWriter out = new FileWriter(outFileName)) {
-                Convert(in, out, c);
+                convert(in, out, c);
             }
         }
     }
 
-    public static void ConvertBuffered(String inFileName, String outFileName, CharConverter c) throws IOException {
+    public static void convertBuffered(String inFileName, String outFileName, CharConverter c) throws IOException {
         try (BufferedReader in = new BufferedReader(new FileReader(inFileName))) {
             try (BufferedWriter out = new BufferedWriter(new FileWriter(outFileName))) {
-                Convert(in, out, c);
+                convert(in, out, c);
             }
         }
     }
